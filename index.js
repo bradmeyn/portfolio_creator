c33c1339e6d77e0f7d81183b83f49c8844426ff1
Brad Meyn <bradmeyn@Brads-MacBook-Air.local>
2021-01-09 17:15:03 +1100

    added full funcationality to each row and added basic table

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
+// const dropdownResults = document.querySelectorAll(".dropdown__results");
+// const searchInputs = document.querySelectorAll(".input--search");
+
+// const dropdownMenu = document.querySelector(".dropdown__menu");
 
 
-
-// const dropdownContent = document.querySelectorAll("dropdown__content");
-const dropdownResults = document.querySelectorAll(".dropdown__results");
-const searchInputs = document.querySelectorAll(".input--search");
-const dropdowns = document.querySelectorAll(".dropdown");
-const dropdownMenu = document.querySelector(".dropdown__menu");
+const table = document.querySelector(".table");
 
 
 const onInput = event => {
+
+    let dropdowns = document.querySelectorAll(".dropdown");
     // console.log(event.target.closest("td").nextSibling);
     dropdowns.forEach(dropdown => {
+        console.log("dropdown");
         dropdown.classList.remove("dropdown__active");
     });
     
@@ -49,14 +51,23 @@ const onInput = event => {
     }
 };
 
-//add onInput to each search with menu delay
-searchInputs.forEach(input => {
-    input.addEventListener("input", debounce(onInput, 500));
+
+//update currency inputs
+table.addEventListener("input", (e) => {
+
+    if(e.target.classList.contains("input--search")){
+
+        onInput(e);
+        updateCost(e.target);
+        updateTotals();
+    }  
 });
+
 
 
 // remove active class when document is clicked
 document.addEventListener("click", event => {
+    let dropdowns = document.querySelectorAll(".dropdown");
     dropdowns.forEach(dropdown => {
         dropdown.classList.remove("dropdown__active");
     });
@@ -64,19 +75,128 @@ document.addEventListener("click", event => {
 
 
 
-const currencyInputs = document.querySelectorAll(".input--currency");
+//update currency inputs
+table.addEventListener("change", (e) => {
 
-currencyInputs.forEach(input => {
-    
-    input.addEventListener("change",(e) => {
-        console.log(e.target);
+    if(e.target.classList.contains("input--currency")){
         updateCost(e.target);
-        input.value = toCurrency(input.value);
+        e.target.value = toCurrency(e.target.value);
+        updateCost(e.target);
         updateTotals();
-    // console.log(input.value * parseFloat(input.closest("tr").children[4].innerHTML));
-    });
+    }  
+});
+
+
+
+let ctx = document.getElementById("main-chart");
+Chart.defaults.global.defaultFontColor = "#2f394b";
+
+Chart.defaults.global.defaultFontStyle = "bold";
+
+Chart.scaleService.updateScaleDefaults("linear", {
+  ticks: {
+    min: 0,
+  },
+});
+
+
+
+let chart = new Chart(ctx, {
+  // The type of chart we want to create
+
+  type: "bar",
+
+  // The data for our dataset
+  data: {
+    datasets: [{
+        label: "Initial Investment",
+        data: [],
+        backgroundColor: "#113C6C",
+        width: "100%",
+
+      },
+    ],
+  },
+
+  // Configuration options go here
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    legend: {
+      display: true,
+      position: "bottom"
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Year'
+        },
+        stacked: true,
+      }],
+
+      yAxes: [{
+
+
+        stacked: true,
+        ticks: {
+          maxTicksLimit: 6,
+          beginAtZero: true,
+          fontSize: 16,
+          callback: function (value, index, values) {
+            if (parseInt(value) >= 1000) {
+              return (
+                "$" + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              );
+            } else {
+              return "$" + value;
+            }
+          },
+        },
+      }, ],
+    },
+    legend: {
+      display: false,
+    },
+    chart: {
+
+    },
+  }
 });
 
 
 
 
+let compound = function () {
+
+    let years = [1,2,3,4,5,6,7,8,9,10];
+    let portfolio = totalAmount;
+    let cost = 1000;
+    let total = 0;
+  
+    let x = 0;
+    let n = 10;
+    let ongoingCosts = [];
+
+    while (x < n) {
+  
+      //Push initial value to the chart
+      
+
+      total = total + cost;
+      ongoingCosts.push(total);
+      x++;
+    }
+  
+  
+    chart.data.datasets[0].data = ongoingCosts;
+    
+    chart.data.labels = years;
+    chart.update();
+  
+  };
+
+  compound();
--- utils.js
+++ utils.js
@@ -1,19 +1,18 @@
 
-const rows = document.querySelectorAll("tr");
+
 const totalAmount = document.getElementById("totalAmount");
 const totalWeight = document.getElementById("totalWeight");
 const totalCostPercent = document.getElementById("totalCostPercent");
 const totalCostDollar = document.getElementById("totalCostDollar");
 
-const deleteBtns = document.querySelectorAll(".delete-btn");
+let deleteBtns = document.querySelectorAll(".delete-btn");
+const addBtn = document.querySelector(".add-btn");
 
-deleteBtns.forEach(btn => {
-    btn.addEventListener("click", e => {
-        e.target.closest("tr").remove();
-    });
-});
+let tBody = document.querySelector("tbody");
+
+
+
 //delay input search
-
 const debounce = (func, delay) => {
     let timeoutId;
 
@@ -28,16 +27,14 @@ const debounce = (func, delay) => {
 };
 
 //convert to currency
-
 const toCurrency = number => {
 
-return numeral(number).format('$0,0.00');
+    return numeral(number).format('$0,0.00');
 
 }
 
 
 //convert to int
-
 const toNumber = value => {
     return numeral(value).value();
     
@@ -45,19 +42,18 @@ const toNumber = value => {
 
 
 //update cost
-
 const updateCost = (amount) => {
     amount.closest("tr").children[5].innerHTML = toCurrency(toNumber(amount.closest("tr").children[2].firstElementChild.value) * toNumber(amount.closest("tr").children[4].innerHTML));
  }
 
 //update totals row
-
  const updateTotals = () => {
+    let rows = document.querySelectorAll("tr");
     let portfolioTotal = 0;
     let costTotal = 0;
     
     rows.forEach(row => {
-
+        
         if(row.closest("tbody")){
             portfolioTotal = portfolioTotal + numeral(row.children[2].children[0].value).value();
             costTotal = costTotal + numeral(row.children[5].innerText).value();
@@ -87,5 +83,74 @@ const updateCost = (amount) => {
     })
  }
 
-
+// delete button functionality
+tBody.addEventListener("click", (e)=> {
+    let selector = "i";
+    // console.log(e.target.innerHTML.includes(selector))
+    if(e.target.closest(".delete-btn")){
+        e.target.closest("tr").remove();
+        console.log("hey")
+        updateTotals();
+        return;
+    };
+  
+}, true);
  
+
+//add button functionality
+addBtn.addEventListener("click", () => {
+    const tableBody = document.querySelector("tbody");
+    const row = document.createElement("tr");
+    row.innerHTML = `<td class="table__item--search">
+    <i class="las la-search search-icon"></i>
+    <input type="text" class="input--search" placeholder="Search Investment">
+    <div class="dropdown">
+        <ul class="dropdown__menu">
+        </div>
+    </div>
+</td>
+<td class="table__item code"></td> 
+<td class="table__item--amount"><input type="text" class="input input--currency" placeholder="Amount"></td>
+<td class="table__item weight">0%</td>
+<td class="table__item cost__percentage">0.00%</td>
+<td class="table__item cost__currency">$0.00</td>
+<td class="table__item--delete"><button class="delete-btn"><i class="lar la-trash-alt"></i>Remove</button></td>`;
+
+    tableBody.appendChild(row);
+    
+});
+
+
+// //update currency inputs
+// table.addEventListener("input", (e) => {
+
+//     if(e.target.classList.contains("input--search")){
+
+//         onInput(e);
+//         updateCost(e.target);
+//         updateTotals();
+//     }  
+// });
+
+
+
+// // remove active class when document is clicked
+// document.addEventListener("click", event => {
+//     let dropdowns = document.querySelectorAll(".dropdown");
+//     dropdowns.forEach(dropdown => {
+//         dropdown.classList.remove("dropdown__active");
+//     });
+// });
+
+
+
+// //update currency inputs
+// table.addEventListener("change", (e) => {
+
+//     if(e.target.classList.contains("input--currency")){
+//         updateCost(e.target);
+//         e.target.value = toCurrency(e.target.value);
+//         updateCost(e.target);
+//         updateTotals();
+//     }  
+// });
